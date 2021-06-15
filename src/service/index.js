f50a3edac22501ffb14999d569daeccc0123a6ae
Enigana <zk@seadora.com.ua>
2021-06-15 23:25:30 +0300

    service add await

--- src/service/index.js
+++ src/service/index.js
@@ -3,6 +3,6 @@ import axios from "axios";
 
 export const getFilms = async (text) => {
 	return text ?
-		axios.get(`https://www.omdbapi.com/?s=${text}&type=movie&r=json&apikey=fabd0e57`) :
-		new Promise(resolve => resolve(data))
+		await axios.get(`https://www.omdbapi.com/?s=${text}&type=movie&r=json&apikey=fabd0e57`) :
+		await new Promise(resolve => resolve(data))
 };
