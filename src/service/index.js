43b977e851a5027638cecf5a9799d0c6a569b5cf
Enigana <zk@seadora.com.ua>
2021-06-15 23:22:07 +0300

    simplify service

--- src/service/index.js
+++ src/service/index.js
@@ -2,8 +2,7 @@ import data from "./initial_data";
 import axios from "axios";
 
 export const getFilms = async (text) => {
-	if (text) {
-		return await axios.get(`https://www.omdbapi.com/?s=${text}&type=movie&r=json&apikey=fabd0e57`)
-	}
-	return await new Promise(resolve => resolve(data))
+	return text ?
+		axios.get(`https://www.omdbapi.com/?s=${text}&type=movie&r=json&apikey=fabd0e57`) :
+		new Promise(resolve => resolve(data))
 };
